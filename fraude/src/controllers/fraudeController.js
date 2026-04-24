22fccb95db10281629380bbe4b0331133f5576dd
Sabrina Massola <sabrinamassola00@gmail.com>
2026-04-24 14:31:27 -0300

    refactor: improve fraud validation controller

--- fraude/src/controllers/fraudeController.js
+++ fraude/src/controllers/fraudeController.js
@@ -1,68 +1,63 @@
 const ordersMock = require('../mocks/orderResponseMocks.json');
 
-const valorfraude = 10; // Máximo de órdenes o transacciones permitidas por día
-const montoMaximo = 10000; // Monto acumulado máximo permitido
+const valorfraude = 10; 
+const montoMaximo = 10000; 
 
-// Algoritmo 1: Detección de fraude por cantidad de órdenes en un día (Órdenes)
+//Detección de fraude por cantidad de órdenes en un día
 const detectarFraudePorFecha = (orders, orderDate) => {
   const now = orderDate;
-  //const now = new Date().toISOString().split('T')[0];  // Fecha actual en formato YYYY-MM-DD
   let contador_fraude = 0;
 
   for (let order of orders) {
-    const orderDate = order.created_at.split('T')[0];  // Obtenemos solo la fecha de la orden
+    const orderDate = order.created_at.split('T')[0]; 
     console.log(`Comparando fecha de orden: ${order.created_at.split('T')[0]} con la fecha actual: ${now}`);
     if (orderDate === now) {
       contador_fraude++;
       console.log(`Orden detectada: ${order.order_id}, Contador de fraude: ${contador_fraude}`);
       if (contador_fraude > valorfraude) {
         console.log('Fraude detectado por cantidad de órdenes.');
-        return true; // Fraude detectado
+        return true; 
       }
     }
   }
   console.log('No se detectó fraude por cantidad de órdenes.');
-  return false; // No se detectó fraude
+  return false; 
 };
 
-// Algoritmo 2: Detección de fraude por monto acumulado de ventas
+// Detección de fraude por monto acumulado de ventas
 const detectarFraudePorMonto = (orders, orderDate) => {
   const now = orderDate;
-  //const now = new Date().toISOString().split('T')[0];  // Fecha actual en formato YYYY-MM-DD
   let acum_precio = 0;
   let cont_fecha = 0;
 
   for (let order of orders) {
-    const orderDate = order.created_at.split('T')[0];  // Obtenemos solo la fecha de la orden
+    const orderDate = order.created_at.split('T')[0];
     if (orderDate === now && order.status === 'approved') {
       acum_precio += order.price;
       cont_fecha++;
       console.log(`Monto acumulado: ${acum_precio}, Contador de órdenes: ${cont_fecha}`);
       if (cont_fecha > valorfraude || acum_precio > montoMaximo) {
         console.log('Fraude detectado por monto acumulado o cantidad de órdenes.');
-        return true; // Fraude detectado
+        return true; 
       }
     }
   }
   console.log('No se detectó fraude por monto acumulado.');
-  return false; // No se detectó fraude
+  return false; 
 };
 
-// Controlador para verificar fraude
+
 const verificarFraude = async (req, res) => {
   try {
     const { user_id } = req.params;
     const { order_date: orderDate } = req.query;
 
-    // Simular respuesta de API externa usando mocks
     console.log("Datos de órdenes:", JSON.stringify(ordersMock));
     const orders = ordersMock.data;
 
-    // Aplicar ambos algoritmos de detección de fraude
     const esFraudePorFecha = detectarFraudePorFecha(orders , orderDate);
-    const esFraudePorMonto = detectarFraudePorMonto(orders);
+    const esFraudePorMonto = detectarFraudePorMonto(orders , orderDate);
 
-    // Determinar si se detecta fraude
     const esFraude = esFraudePorFecha || esFraudePorMonto;
     res.status(200).json({ is_fraud: esFraude });
   } catch (error) {
