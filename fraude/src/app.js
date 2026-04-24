a82f3a655aace7e3684e2077962ef2d95308836f
Sabrina Massola <sabrinamassola00@gmail.com>
2026-04-24 14:25:16 -0300

    refactor: clean app server setup

--- fraude/src/app.js
+++ fraude/src/app.js
@@ -1,17 +1,13 @@
-// Importar el controlador de fraude
 const { verificarFraude } = require('./controllers/fraudeController');
 
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Middleware para analizar JSON (opcional, si necesitas manejar POST requests con datos en JSON en el futuro)
 app.use(express.json());
 
-// Endpoint GET para verificar fraude de un usuario específico
 app.get('/api/v1/validate/:user_id', verificarFraude);
 
-// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Microservicio de ventas y detección de fraude en el puerto ${PORT}`);
 });
